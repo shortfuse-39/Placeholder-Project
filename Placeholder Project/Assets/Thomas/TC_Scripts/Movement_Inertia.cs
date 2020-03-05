b190aa12e31ff102daab9888ca73ffa8e20e4bdc
shortfuse-39 <58072539+shortfuse-39@users.noreply.github.com>
2020-03-05 21:38:31 +0000

    Collectables and Managers
    Need placing into the main scene for testing (JIC)

---
+++ Placeholder Project/Assets/Thomas/TC_Scripts/Movement_Inertia.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Inertia : MonoBehaviour
+{
+    // https://forum.unity.com/threads/best-player-movement-script-in-c-3d.435803/
+    public float MovementSpeed;
+    private Rigidbody rigidBody;
+       
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rigidBody.velocity += transform.TransformDirection(Vector3.forward) * Time.deltaTime * MovementSpeed;
+            Debug.Log("Forward!");
+        }
+        else if (Input.GetKey("s"))
+        {
+            rigidBody.velocity -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * MovementSpeed;
+            Debug.Log("Back!");
+        }
+
+        if (Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            rigidBody.velocity += transform.TransformDirection(Vector3.left) * Time.deltaTime * MovementSpeed;
+            Debug.Log("Left!");
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            rigidBody.velocity -= transform.TransformDirection(Vector3.left) * Time.deltaTime * MovementSpeed;
+            Debug.Log("Right!");
+        }
+    }
+}
