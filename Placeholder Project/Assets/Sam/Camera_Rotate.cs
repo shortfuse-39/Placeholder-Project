a4e3390faec475a037bd95c6f3cddaa4d2ee25ce
Sam - Tilki <58072569+Jump-fox@users.noreply.github.com>
2020-03-20 18:01:58 +0000

    Not my fault if this breaks everything :) p.s blame Thomas

---
+++ Placeholder Project/Assets/Sam/Camera_Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Rotate : MonoBehaviour
+{
+
+    public float speed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the object around its local Y axis
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+}
